32e8af163141f20b1529b1f02b7af171077809e1
kalpadhwaryu <kalpadhwaryu1581@gmail.com>
2022-02-03 08:53:22 +0530

    Dev commit 1

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="inner-content">
+          <h3 className="footer">
+            Made by <a href="https://github.com/kalpadhwaryu">Kalp Adhwaryu</a>
+          </h3>
+        </div>
+      </div>
+    </footer>
+  );
+};
